bc8776df566099adb7003bd386d0a45436229eaf
guan xin <guanxin@tinytiger.cn>
2020-11-13 15:50:20 +0800

    修改api接口

--- utils/api.js
+++ utils/api.js
@@ -11,31 +11,26 @@ import {get, post, put, del} from './request';
  */
 export const API_ROOT = 'https://www.doniai.com';
 
+export const getOpenid = (data) => post(`${API_ROOT}/api/user/login`, data);
+export const authLogin = (data) => post(`${API_ROOT}/api/user/auth`, data);
+
+export const getPhotoHistory = () => get(`${API_ROOT}/api/user/photo_history`);
+
+export const getQuestions = () => get(`${API_ROOT}/api/question`);
+export const getQuestion = (question_id) => get(`${API_ROOT}/api/question/detail?question_id=${question_id}`);
+
+export const getOrders = (status) => get(`${API_ROOT}/api/order?status=${status}`);
+export const getOrder = (order_id) => get(`${API_ROOT}/api/order/detail?order_id=${order_id}`);
+
+export const getAddresses = () => get(`${API_ROOT}/api/address`);
+export const getAddress = (address_id) => get(`${API_ROOT}/api/address/detail?address_id=${address_id}`);
+export const createAddress = (data) => post(`${API_ROOT}/api/address/create`, data);
+export const updateAddress = (data) => post(`${API_ROOT}/api/address/edit`, data);
 
 
+export const getTranslate = (data) => post(`${API_ROOT}/api/ai/translate`, data);
 
-/*********************************************************************************************公共通用方法接口都POST请求*********************************************************************/
-
-/**
- * 说明/请求：判断是否弹出视频 post 传uid用户主键
- * 地址/返回：Api/Index/isTan   0是弹出 1不弹 
- * @param {object} data 
- */
-export const isTan = (data) => post(`${API_ROOT}/Api/Index/isTan`, data);
-
-/**
- * 看完视频保存有效期 post 传uid
- * Api/Index/saveType 1成功 0失败
- * @param {object} data 
- */
-export const saveType = (data) => post(`${API_ROOT}/Api/Index/saveType`, data);
-
-/**
- * 首页展示 GET
- * Api/Index/index 返回$ggtop轮播图， num 使用次数
- */
-export const getSwiperImage = () => get(`${API_ROOT}/Api/Index/index`);
-
+export const getTranslate = (data) => post(`${API_ROOT}/api/ai/translate`, data);
 
 
 /*********************************************************************************************登录体系*********************************************************************/
@@ -60,108 +55,3 @@ export const authlogin = (data) => post(`${API_ROOT}/Api/Login/authlogin`, data)
  * @param {object} data 
  */
 export const saveUserInfo = (data) => post(`${API_ROOT}/Api/Login/login`, data);
-
-
-
-/*********************************************************************************************几种图片上传体系*********************************************************************/
-
-/**
- * 切图的（包含9宫格和4宫格），图片上传 （上传参数请看我发的小程序那个）
- * Api/Uploade/upload  返回img 则是图片地址
- * @param {object} data 
- */
-export const cropImageUpload = (data) => post(`${API_ROOT}/Api/Uploade/upload`, data);
-
-/**
- * 普通识别的图片上传
- * Api/Uploade/uploadp
- * @param {object} data 
- */
-export const simpleImageUpload = (data) => post(`${API_ROOT}/Api/Uploade/uploadp`, data);
-
-/**
- * 精准识别的图上传
- * Api/Uploade/uploads
- * @param {object} data 
- */
-export const preciseImageUpload = (data) => post(`${API_ROOT}/Api/Uploade/uploads`, data);
-
-/**
- * 颜值图上传
- * Api/Uploade/uploadyz
- * @param {object} data 
- */
-export const yzImageUpload = (data) => post(`${API_ROOT}/Api/Uploade/uploadyz`, data);
-
-
-
-/*********************************************************************************************切图体系*********************************************************************/
-
-/**
- * 九宫格切图和四宫格切图 传uid url(图片路径) type=9则是九宫格 =4则是四宫格
- * Api/Index/save 
- * 返回： 如果是九宫格则返回$arrs1 $arrs2 $arrs3  四宫格则是$arrs1 $arrs2 (3个变量是3行，4个变量是4行)
- * @param {object} data 
- */
-export const cropGongge = (data) => post(`${API_ROOT}/Api/Index/save`, data);
-
-
-
-/*********************************************************************************************文字识别和处理体系*********************************************************************/
-
-/**
- * 百度识别图片  传img（刚刚上传图片返回地址直接给我即可）
- * Api/Uploade/ocr   返回1 成功 返回0请显示后台返回的err字段.
- * @param {object} data 
- */
-export const baiduImageOcr = (data) => post(`${API_ROOT}/Api/Uploade/ocr`, data);
-
-
-/**
- * 用户点击复制则调用此接口，保存内容   uid  str(是识别的内容)
- * Api/Uploade/save
- * @param {object} data 
- */
-export const getOcrContent = (data) => post(`${API_ROOT}/Api/Uploade/save`, data);
-
-/**
- * 翻译  传参 str
- * Api/Uploade/translate
- * @param {object} data 
- */
-export const getTranslateText = (data) => post(`${API_ROOT}/Api/Uploade/translate`, data);
-
-/**
- * 返回最新100条识别记录列表 POST 传uid
- * Api/Index/historyList 返回 data数据
- * @param {object} data 
- */
-export const getOcrHistoryList = (data) => post(`${API_ROOT}/Api/Index/historyList`, data);
-
-/**
- * 一条识别记录的详情 post 传记录id
- * Api/Index/getContent
- * @param {object} data 
- */
-export const getOcrHistoryDetail = (data) => post(`${API_ROOT}/Api/Index/getContent`, data);
-
-/**
- * 更新识别的文字  传记录id 以及文字str
- * Api/Index/update
- * @param {object} data 
- */
-export const updateOcrText = (data) => post(`${API_ROOT}/Api/Index/update`, data);
-
-/**
- * 批量删除记录 传ids(是多个记录id用英文逗号分隔)
- * Api/Index/del
- * @param {object} data 
- */
-export const batchDeleteNote = (data) => post(`${API_ROOT}/Api/Index/del`, data);
-
-/**
- * 多条记录合并  传ids(是多个记录id用英文逗号分隔)和uid
- * Api/Index/saves
- * @param {object} data 
- */
-export const mergeNote = (data) => post(`${API_ROOT}/Api/Index/saves`, data);

b8aae24b182245d17c0b0cb0ec870e76e2093470
guan xin <guanxin@tinytiger.cn>
2020-11-18 13:52:21 +0800

    完成图片制作预览页

---
+++ pages/photo/preview.js
@@ -0,0 +1,103 @@
+// pages/photo/preview.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    CustomBar: app.globalData.CustomBar,
+    preview_image_url: 'http://doniai-mini.oss-cn-shenzhen.aliyuncs.com/4fb53d05a76c97e25fb1ebc14b6ea260.jpg',
+    checkbox: [{
+      value: 0,
+      name: '个性换装 | 5.99元',
+      checked: false,
+      hot: false,
+    }, {
+      value: 1,
+      name: '3种底色 | 1.99元',
+      checked: true,
+      hot: true,
+    }]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  ChooseCheckbox(e) {
+    let items = this.data.checkbox;
+    let values = e.currentTarget.dataset.value;
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      if (items[i].value == values) {
+        items[i].checked = !items[i].checked;
+        break
+      }
+    }
+    this.setData({
+      checkbox: items
+    })
+  }
+})
